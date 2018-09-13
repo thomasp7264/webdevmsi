e01ec2fc7a1a83141ec4727424e03c302205fee4
witusj <wfth69@gmail.com>
2018-09-13 20:10:50 +0200

    Update

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("hello").style.fontSize = "200px";
   } else {
     document.getElementById("hello").style.fontSize = "120px";
